66718e6ac5f222bca270e5096d97a6c3bc135ae2
pphatdev <pphat.kits@gmail.com>
2025-08-05 14:01:07 +0700

    add list.js for listing files with links from MEGA storage

---
+++ list.js
@@ -0,0 +1,39 @@
+import { storage } from "./config.js";
+
+async function listFilesWithLinks(folder, prefix = '') {
+    for (const item of folder.children) {
+        const path = `${prefix}${item.name}`;
+        if (item.directory) {
+            await listFilesWithLinks(item, `${path}/`);
+        } else {
+            // Get the full public link for the file
+            const link = await item.link();
+            console.log(`${path}: ${link}`);
+        }
+    }
+}
+
+if (process.argv.slice(2).length === 0) {
+    console.log('Usage: node list.js <root|test>');
+    process.exit(1);
+}
+
+
+process.argv.slice(2).forEach(async (arg) => {
+    if (arg === 'root') {
+        await listFilesWithLinks(storage.root);
+        process.exit(0);
+    }
+    else if (arg === 'test') {
+        const testFolder = storage.root.children.find(item => item.name === 'test' && item.directory);
+        if (testFolder) {
+            await listFilesWithLinks(testFolder);
+            process.exit(0);
+        } else {
+            console.log('Folder "test" not found in the root directory');
+        }
+    } else {
+        console.log(`Unknown argument: ${arg}`);
+        process.exit(1);
+    }
+});
